5222c93fed3a0ad89d72040264de43bc2cb61aa7
nelly <nellybulatova12@gmail.com>
2024-06-04 10:16:57 +0300

    add game display

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const buttons = document.querySelectorAll("#button")
+const resultField = document.querySelector("#resultField")
+const computerScoreField = document.querySelector("#computerScoreField")
+const humanScoreField = document.querySelector("#humanScoreField")
+
 let HumanScore = 0;
 let computerScore = 0;
 
@@ -12,85 +17,84 @@ function getComputerChoice() {
     }
 }
 
-// function getHumanChoice() {
-//     const input = prompt("choose rock/paper/scissors") 
-//     return input
-// }
+function displaySelection(result) {
+    if (!resultField.hasChildNodes()) {
+    } else {
+        resultField.textContent = ""
+    }
+    resultField.textContent = result
+}
+
+function updateScore() {
+    computerScoreField.textContent = computerScore
+    humanScoreField.textContent = HumanScore
+
+    if (HumanScore === 5 || computerScore === 5) {
+        resultField.textContent = HumanScore > computerScore ? "Human won!!!" : "Computer won!!!"
+    }
+}
+
 
 function playRound(human) {
-    // const human = getHumanChoice()
     const computer = getComputerChoice()
+    let message = ""
     if (human === "rock") {
         if (computer === "rock") {
-            return `human - ${human}, computer - ${computer}, so it's a tie`
+            message = `human - ${human}, computer - ${computer}, so it's a tie`
         } else if (computer === "scissors") {
-            return `human - ${human}, computer - ${computer}, so human wins`
+            message = `human - ${human}, computer - ${computer}, so human wins`
+            HumanScore++
         } else {
-            return `human - ${human}, computer - ${computer}, so computer wins`
+            message = `human - ${human}, computer - ${computer}, so computer wins`
+            computerScore++
         }
     }
     else if (human === "paper") {
         if (computer === "paper") {
-            return `human - ${human}, computer - ${computer}, so it's a tie`
+            message = `human - ${human}, computer - ${computer}, so it's a tie`
         } else if (computer === "rock") {
-            return `human - ${human}, computer - ${computer}, so human wins`
+            message = `human - ${human}, computer - ${computer}, so human wins`
+            HumanScore++
         } else {
-            return `human - ${human}, computer - ${computer}, so computer wins`
+            message = `human - ${human}, computer - ${computer}, so computer wins`
+            computerScore++
         }
     } 
     else if (human === "scissors") {
         if (computer === "scissors") {
-            return `human - ${human}, computer - ${computer}, so it's a tie`
+            message = `human - ${human}, computer - ${computer}, so it's a tie`
         } else if (computer === "paper") {
-            return `human - ${human}, computer - ${computer}, so human wins`
+            message = `human - ${human}, computer - ${computer}, so human wins`
+            HumanScore++
         } else {
-            return `human - ${human}, computer - ${computer}, so computer wins`
+            message = `human - ${human}, computer - ${computer}, so computer wins`
+            computerScore++
         }
     }
-    return "ERROR"
+    displaySelection(message)
+    updateScore()
 
 }
 
-// console.log(playRound())
 
-
-// const container = document.querySelector("#container");
-
-// const content = document.createElement("div")
-// content.classList.add("content")
-// content.textContent = "Text content"
-
-// const paragraph = document.createElement("p")
-// paragraph.style.color = "red"
-// paragraph.textContent = "I'm red"
-
-// container.appendChild(content)
-// container.appendChild(paragraph)
-
-const buttons = document.querySelectorAll("#button")
 
 buttons.forEach(button => {
     button.addEventListener("click", e => {
         switch (e.target.textContent) {
             case "Rock":
-                // console.log("it's rock")
-                console.log(playRound("rock"))
-                break;
+                playRound("rock")
+             break;
             case "Paper":
-                // console.log("it's paper")
-                console.log(playRound("paper"))
-                break;
+            playRound("paper")
+              break;
             case "Scissors":
-                // console.log("it's scissors")
-                console.log(playRound("scissors"))
+               playRound("scissors")
                 break;
             
-            // default:
-            //     console.log("error, no such button")
+            default:
+                console.log("error, no such button")
         }
-        // console.log(e.target.textContent === "Rock")
     })
 })
 
-// console.log(buttons)
 
